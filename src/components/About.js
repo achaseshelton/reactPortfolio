5e3beabe3b798449c074404d9e6e09c910f3870a
austen.shelton <austen.shelton@gmail.com>
2021-10-22 08:09:13 -0400

    styling

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
                </p>
             </div>
             <div className="d-flex justify-content-center">
-            <img src="./img/disney.jpg"></img>
+            <img src="./img/disney.jpg" className="image-fluid"></img>
             </div>
         </div>
     )
